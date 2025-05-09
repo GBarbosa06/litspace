99beb7556aa6cae6a66e09ce682d2749b9560610
GBarbosa <andradegui2006@gmail.com>
2025-05-08 21:10:44 -0300

    project base
    Vite init + firebase inicial config + router + tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Lit <span>Space</span></h1>
+
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<h2>Home</h2>} />
+            <Route path="/about" element={<h2>About</h2>} />
+            <Route path="/contact" element={<h2>Contact</h2>} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+          <div className="footer">
+            <p>Lit Space &copy; 2023</p>
+            <p>All rights reserved</p>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
