bc4bee32292660b53f1252ab37b86bbba5fa4983
GBarbosa <andradegui2006@gmail.com>
2025-05-10 18:01:06 -0300

    useAuthentication hook + register page working

---
+++ src/hooks/useAuthentication.jsx
@@ -0,0 +1,50 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, signOut } from 'firebase/auth';
+import { db } from '../firebase/config';
+import { app } from '../firebase/config';
+import { useState, useEffect, use } from 'react';
+
+export const useAuthentication = () => {
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+    const [cancelled, setCancelled] = useState(false);
+
+    useEffect(() => {
+        return () => setCancelled(true);
+    }, []);
+
+    const auth = getAuth(app);
+
+    const checkIfIsCancelled = () => {
+        if (cancelled) {
+            return;
+        }
+    };
+
+    const createUser = async (data) => {
+        checkIfIsCancelled();
+        setLoading(true);
+        setError(null);
+
+        try {
+            const { user } = await createUserWithEmailAndPassword(auth, data.email, data.password);
+            await updateProfile(user, { displayName: data.displayName });
+            setLoading(false);
+            return user;
+        } catch (error) {
+            console.log(error.message);
+            let systemErrorMEssage;
+            if(error.message.includes("Password")){
+                systemErrorMEssage = "A senha precisa conter pelo menos 6 caracteres.";
+            }
+            else if(error.message.includes("email-already")){
+                systemErrorMEssage = "E-mail já cadastrado";
+            }
+            else{
+                systemErrorMEssage = "Ocorreu um erro, por favor tente mais tarde."
+            }
+            setError(systemErrorMEssage);
+            setLoading(false);
+        }
+    };
+    return { auth, createUser, error, loading };
+}
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,4 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+// hooks
+import { useAuthentication } from '../../hooks/useAuthentication';
+
+//components
 import Input from '../../components/Input'
 import Label from '../../components/Label';
 
@@ -9,12 +15,34 @@ const Register = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState(null);
 
+  const { createUser, error: authError, loading } = useAuthentication();
+  const navigate = useNavigate()
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(null);
+    const user = {
+      displayName,
+      email,
+      password
+    }
+    if (password !== confirmPassword) {
+      setError('As senhas são diferentes');
+      return;
+    }
 
+    const res = await createUser(user);
+    if (res) {
+      navigate('/');
+    }
     
   }
+
+  useEffect(() =>{
+    if(authError){
+      setError(authError)
+    }
+  }, [authError])
 
   return (
     <div className='flex flex-col mt-10 items-center h-screen '>
@@ -25,6 +53,7 @@ const Register = () => {
           <span className=' text-[#ccc] font-bold'>Nome: </span>
           <Input 
           type="text" 
+          name="displayName"
           placeholder="Nome do usuário" 
           onChange={(e) => setDisplayName(e.target.value)}
           value={displayName}
@@ -33,7 +62,8 @@ const Register = () => {
       <Label>
           <span className=' text-[#ccc] font-bold'>Email: </span>
           <Input 
-          type="text" 
+          type="email" 
+          name="email"
           placeholder="Digite seu email" 
           onChange={(e) => setEmail(e.target.value)}
           value={email}
@@ -42,7 +72,8 @@ const Register = () => {
       <Label>
           <span className=' text-[#ccc] font-bold'>Senha: </span>
           <Input 
-          type="text" 
+          type="password" 
+          name="password"
           placeholder="Digite sua senha" 
           onChange={(e) => setPassword(e.target.value)}
           value={password}
@@ -51,13 +82,15 @@ const Register = () => {
       <Label>
           <span className=' text-[#ccc] font-bold'>Confirme sua senha: </span>
           <Input 
-          type="text" 
+          type="password" 
+          name="confirmPassword"
           placeholder="Repita senha"
           onChange={(e) => setConfirmPassword(e.target.value)} 
           value={confirmPassword}
           required />
       </Label>
-      <button className='btn'>Cadastrar</button>
+      {!loading && <button className='btn'>Cadastrar</button>}
+      {loading && <p className='text-[#ccc]'>Aguarde...</p>}
       {error && <p className='error'>{error}</p>}
       <p className='text-sm text-[#696969]'>Já tem uma conta? <a href="/login" className='text-[#ccc] font-bold'>Faça login</a></p>
     </form>
