1a959a1ab75f2bd18df288bff25b41f8d6e813f9
GBarbosa <andradegui2006@gmail.com>
2025-05-12 19:54:01 -0300

    small fixes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div className='flex flex-col mt-10 items-center h-screen '>
         <h1 className='text-3xl font-bold'>Lit Space</h1>
-        <p className='text-base text-[#696969] mb-5'>Seja bem-vindo ao Lit Space, um espaço para compartilhar suas leituras e interagir com outros leitores.</p>
+        <p className='text-base text-[#696969] mb-5'>Seja bem-vindo(a) ao Lit Space, um espaço para compartilhar suas leituras e interagir com outros leitores.</p>
         <HomeSearch />
     </div>
   )
