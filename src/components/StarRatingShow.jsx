8defe6169088e472c75b22bbd17b415e6e6fad99
GBarbosa <andradegui2006@gmail.com>
2025-05-16 14:59:41 -0300

    Update StarRatingShow.jsx

--- src/components/StarRatingShow.jsx
+++ src/components/StarRatingShow.jsx
@@ -1,14 +1,16 @@
 import { Star } from 'lucide-react';
 
 export function StarRatingShow({ rating }) {
-  const stars = Array.from({ length: 5 }, (_, i) => (
-    <Star
-      key={i}
-      size={20}
-      fill={i < rating ? '#facc15' : 'none'} // preenche até o rating
-      stroke="#facc15" // contorno amarelo
-    />
-  ));
+  const stars = []
+    for (let i = 0; i < 5; i++) {
+        stars.push(<Star
+        key={i}
+        size={20}
+        fill={i < rating ? '#facc15' : 'none'} // preenche até o rating
+        stroke="#facc15" // contorno amarelo
+      />)
+    
+  }
 
   return <div className="flex gap-1">{stars}</div>;
 }
