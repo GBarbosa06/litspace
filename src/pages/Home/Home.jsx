6f6f65c9f002d424b802734b9d628ea8f7db2a49
GBarbosa <andradegui2006@gmail.com>
2025-05-25 22:22:44 -0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,15 +6,11 @@ import ReviewsList from '../../components/ReviewsList';
 
 const Home = () => {
 
-
-
-  
   return (
     <div className='flex flex-col mt-10 items-center min-h-screen '>
         <h1 className='text-3xl font-bold'>Lit Space</h1>
         <p className='text-base text-[#696969] mb-5'>Seja bem-vindo(a) ao Lit Space, um espaço para compartilhar suas leituras e interagir com outros leitores.</p>
         <HomeSearch />
-
         <h2 className='border-t-1 border-gray-600 w-[80%] m-5 pt-3 text-center text-2xl font-bold text-[#FFD369]'>Últimas avaliações:</h2>
         <ReviewsList />
     </div>
